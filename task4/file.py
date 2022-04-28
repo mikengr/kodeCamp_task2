203cc8bceed7af3282e52aa8cc4fe97fa29f47b5
Michael Rowland N <94012232+mikengr@users.noreply.github.com>
2022-04-28 17:12:24 +0100

    updated file path

--- task4/file.py
+++ task4/file.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 
 @app.post("/files/")
-async def file_upload(file: UploadFile = File(...)):
-    if file.filename == "":
-        raise HTTPException(status_code=406, detail="upload a valid file")
+async def file_upload(file: UploadFile = File(default=None)):
+    if file is None:
+        raise HTTPException(status_code=404, detail="no file uploaded")
     return {
         "name": file.filename,
         "img_type": file.content_type,
